e4d79042ba62b2d1d1ccbeb0043d8f27a75952cb
Steven Cozart <stvn.czrt@gmail.com>
2019-03-15 22:12:25 -0500

    Removed pacDrive LED stuff.

--- runcommand-onend.sh
+++ runcommand-onend.sh
@@ -16,14 +16,6 @@ emulator=$2
 game=$(basename "$3")
 game=${game%.*}
 
-# sudo /home/pi/bin/led-end "$system" "$game" >/dev/null
-sudo /home/pi/bin/led-start  10
-sudo /home/pi/bin/led-start  10,8,13,16
-sudo /home/pi/bin/led-start  10,8,13,16,1,4,12,15
-sudo /home/pi/bin/led-start  10,8,13,16,1,4,12,15,2,5,11,14
-sudo /home/pi/bin/led-start  10,8,13,16,1,4,12,15,2,5,11,14,3,6,9
-sudo /home/pi/bin/led-start  10,8,13,16,1,4,12,15,2,5,11,14,3,6,9,7
-
 #  Send game info to picture server
 python3 /home/pi/bin/simpleClient.py "CLOSE" >/dev/null
 python3 /home/pi/bin/simpleClient.py "OPEN all emulation_station" >/dev/null
