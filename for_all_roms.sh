7a1b8586d776ac819140839c9243d675f3ff1275
Steven Cozart <stvn.czrt@gmail.com>
2019-03-29 19:56:40 -0500

    Create for_all_roms.sh

---
+++ for_all_roms.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+# USAGE:  for_all_roms.sh
+#
+# builds a button_map image for every ROM on your collection.
+
+FILES=/home/pi/RetroPie/roms/arcade/*.zip
+
+for f in $FILES
+do
+   echo "Processing $f file..."
+   rom=$(basename -- $f)
+   rom="${rom%.*}"
+#  echo "BASENAME: $rom"
+   /home/pi/bin/button_map.sh $rom
+done
